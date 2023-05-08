b3a8a07160e7990824034cfc76d8d69c2052a5f4
nick <nicolas.ai.nomis@gmail.com>
2023-05-08 21:44:26 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import reactLogo from './assets/react.svg';
+
+function App() {
+  return (
+    <div>
+      <a href="https://react.dev" target="_blank" rel="noreferrer">
+        <img src={reactLogo} className="h-24 m-auto" alt="React logo" />
+      </a>
+    </div>
+  );
+}
+
+export default App;
